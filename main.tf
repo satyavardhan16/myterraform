63ceb87b02dd704c5356d7d761488ce251271acf
satyavardhan16 <43573878+satyavardhan16@users.noreply.github.com>
2020-12-23 09:50:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0cb0e70f44e1a4bb5"
   instance_type = "t2.micro"
   tags = {
-    Name = "JenkinsSatya22"
+    Name = "JenkinsSatya212"
     }
  }
