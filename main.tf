420b911134c3063a60a5e4803b4704c1281d7c14
satyavardhan16 <43573878+satyavardhan16@users.noreply.github.com>
2020-12-26 13:28:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,50 @@ terraform {
       encrypt = true
     }
 }
+resource "aws_s3_bucket" "terraform_state" {
+    bucket  = "terraform-state-satya"
+  
+  lifecycle {
+    prevent_destroy = true
+    }
+  
+  versioning  {
+    enabled = true
+    }
+  server_side_encryption_configuration_by_default {
+    rule  {
+      apply_server_side_encryption_by_default {
+        see_algorithm = "AES256"
+        }
+      }
+    }
+  }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name  = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key  = "LockID"
+  
+  attribute {
+    name  = "LockID"
+    type  = "S"
+    }
+  }
+    
+  
+  
+resource "aws_db_instance" "default" {
+  allocated_storage = 5
+  storage_type  = "gp2"
+  engine  = "mysql"
+  engine_version  = "5.7"
+  instance_class  = "db.t2.micro"
+  name  = "mydb"
+  username  = "foo"
+  password  = "foopass"
+  parameter_group_name  = "default.mysql5.7"
+  skip_final_snapshot = true
+ }
 
 provider "aws" {
   region = "ap-south-1"
