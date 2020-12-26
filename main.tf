5acd2bc1c9126e25a216633320e84988f80eeecb
satyavardhan16 <43573878+satyavardhan16@users.noreply.github.com>
2020-12-26 18:36:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_db_instance" "default" {
   instance_class  = "db.t2.micro"
   name  = "mydb"
   username  = "foo"
-  password  = "foopass"
+  password  = "qweiop10"
   parameter_group_name  = "default.mysql5.7"
   skip_final_snapshot = true
  }
