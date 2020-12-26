7fa15e94db0854788e760e193a94b7dee6ed2071
satyavardhan16 <43573878+satyavardhan16@users.noreply.github.com>
2020-12-26 12:58:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ terraform {
 }
 
 provider "aws" {
+    backend "s3"  {
+      bucket  = "terraform-state-satya"
+      key = "global/s3/terraform.tfstate"
+      region = "ap-south-1"
+      dynamodb_table  = "terraform-state-locking"
+      encrypt = true
+    }
   region = "ap-south-1"
   version = "~> 2.0"
   shared_credentials_file = "/home/bitnami/.aws/credentials"
